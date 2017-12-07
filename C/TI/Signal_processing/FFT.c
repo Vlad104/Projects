f67a13896a9cbd15177cf4b5434a2ae8f87df082
Vlad104 <vlad104khk@gmail.com>
2017-12-07 18:27:33 +0300

    Write all alghoritm, but it has an errors

--- C/TI/Signal_processing/FFT.c
+++ C/TI/Signal_processing/FFT.c
@@ -1,4 +1,4 @@
-#include "fpu_rfft.h"
+﻿#include "fpu_rfft.h"
 #include "fpu_cfft.h"
 #include "math.h"
 #include "examples_setup.h"
@@ -32,22 +32,37 @@ float CFFToutBuff[CFFT_SIZE];
 #pragma DATA_SECTION(CFFTF32Coef,"CFFTdata4")
 float CFFTF32Coef[CFFT_SIZE];
 
-#pragma DATA_SECTION(A1,"Mat1_2kb")
-float A1[EQ64][EQ256];
-#pragma DATA_SECTION(A2,"Mat2_2kb")
-float A2[EQ64][EQ256];
-#pragma DATA_SECTION(B1,"Mat3_2kb")
-float B1[EQ256][EQ64];
-#pragma DATA_SECTION(B2,"Mat4_2kb")
-float B2[EQ256][EQ64];
-#pragma DATA_SECTION(B1M,"Mat5_1kb")
-float B1M[EQ256][EQ64/2];
-#pragma DATA_SECTION(B2M,"Mat6_1kb")
-float B2M[EQ256][EQ64/2];
-#pragma DATA_SECTION(C,"Mat7_1kb")
-float C[EQ256][EQ64/2];
-#pragma DATA_SECTION(D,"Mat8_1kb")
-Uint32 D[EQ256][EQ64/2];
+#pragma DATA_SECTION(BufferFFTt1,"Mat1_2kb")
+float BufferFFTt1[EQ64][EQ256];
+#pragma DATA_SECTION(BufferFFTt2,"Mat2_2kb")
+float BufferFFTt2[EQ64][EQ256];
+#pragma DATA_SECTION(BufferFFTw1,"Mat3_2kb")
+float BufferFFTw1[EQ256][EQ64];
+#pragma DATA_SECTION(BufferFFTw2,"Mat4_2kb")
+float BufferFFTw2[EQ256][EQ64];
+#pragma DATA_SECTION(BufferFFTw1_mag,"Mat5_1kb")
+float BufferFFTw1_mag[EQ256][EQ64/2];
+#pragma DATA_SECTION(BufferFFTw2_mag,"Mat6_1kb")
+float BufferFFTw2_mag[EQ256][EQ64/2];
+#pragma DATA_SECTION(Energy_0,"Mat7_1kb")
+float Energy_0[EQ256][EQ64/2];
+#pragma DATA_SECTION(Energy,"Mat8_1kb")
+float Energy[EQ256][EQ64/2];
+
+float Angle[EQ256][EQ64/2];
+
+#define MAX_STRUCT_SIZE 110
+#define MAX_STACK_POINTER 100
+
+typedef struct {
+    Uint16 cluster;
+    float energy;
+    float distance;
+    float velocity;
+    float angle;
+} OUT_DATA;
+
+OUT_DATA* Struct[MAX_STRUCT_SIZE];
 
 
 float RadStep = 0.1963495408494f;  // Step to generate test bench waveform
@@ -72,7 +87,9 @@ void signal_init_comp();
 void make_signal_1();
 void make_signal_2();
 void adaptive_threshold();
+void argument();
 void clustering();
+void clustering_init(Uint16 * Stack[][EQ256], Uint16 * Mark[][2]);
 
 Uint32 t1 = 0, t2 = 0, dt = 0;
 
@@ -83,8 +100,6 @@ void main(void)
     signal_init_real();
     signal_init_comp();
 
-    Uint16 i, j;
-
     //Uint32 t1 = 0, t2 = 0, dt = 0;
     //TIME(t1);
     //
@@ -92,162 +107,277 @@ void main(void)
     //dt = t2 - t1;
     //asm(" ESTOP0");
 
-//Step 1: Calculate rfft(264) x64
+    fft_t(); //Step 1: Calculate rfft(264) x64
+
+    fft_w(); //Step 2: Calculate cfft(64) x256
+
+    adaptive_threshold(); //Step 3: Treshold
+
+    clustering(); //Step 4: Clustering
+
+    asm(" ESTOP0");
+    while(1) {};
+}
+
+void fft_t()
+{
+    Uint16 i, j;
     for (i = 0; i < EQ64; i++)
     {
-        make_signal_2();       //input data for A1
-        RFFT_f32(hnd_rfft);
-        for (j = 0; j < EQ256/2; j++) {  //правильно ли?
-            A1[i][2*j] = RFFToutBuff[j];    //complex data for A1
-            A1[i][2*j+1] = RFFToutBuff[EQ256 - 1 - j]; //for bit reversing
-        }
-
-        make_signal_2();         //input data for A2
+        make_signal_1();       //input data for BufferFFTt1
         RFFT_f32(hnd_rfft);
         for (j = 0; j < EQ256/2; j++) {
-            A2[i][2*j] = RFFToutBuff[j];    //complex data for A2
-            A2[i][2*j+1] = RFFToutBuff[EQ256 - 1 - j]; //for bit reversing
+            BufferFFTt1[i][2*j] = RFFToutBuff[j];    //complex data for BufferFFTt1
+            BufferFFTt1[i][2*j+1] = RFFToutBuff[EQ256 - 1 - j]; //for bit reversing
+        }
+
+        make_signal_2();         //input data for BufferFFTt2
+        RFFT_f32(hnd_rfft);
+        for (j = 0; j < EQ256/2; j++) {
+            BufferFFTt2[i][2*j] = RFFToutBuff[j];    //complex data for BufferFFTt2
+            BufferFFTt2[i][2*j+1] = RFFToutBuff[EQ256 - 1 - j]; //for bit reversing
         }
     }
-    //asm(" ESTOP0");
-//Step 2: Calculate cfft(64) x256
-    Uint16 ii = 0;
+}
+
+void fft_w()
+{
+    Uint16 i, j, ii = 0;
     for (i = 0; i < EQ256; i++)
     {
         ii = 2*i;
 
-        for (j = 0; j < EQ64; j++) {      //input data for B1
-            CFFTin1Buff[j] = A1[j][ii];
-            CFFTin1Buff[j+1] = A1[j][ii+1];
+        for (j = 0; j < EQ64; j++) {      //input data for BufferFFTw1
+            CFFTin1Buff[j] = BufferFFTt1[j][ii];
+            CFFTin1Buff[j+1] = BufferFFTt1[j][ii+1];
         }
 
         CFFT_f32(hnd_cfft);                    // Calculate FFT
         CFFT_f32_mag(hnd_cfft);
 
         for (j = 0; j < EQ64; j++)             // save results
-            B1[i][j] = hnd_cfft->CurrentInPtr[j];
+            //BufferFFTw1[i][j] = hnd_cfft->CurrentInPtr[j];
+            BufferFFTw1[j][i] = hnd_cfft->CurrentInPtr[j];
 
         for (j = 0; j < EQ64/2; j++)
-            B1M[i][j] = hnd_cfft->CurrentOutPtr[j];  //magnitude data
+            //BufferFFTw1_mag[i][j] = hnd_cfft->CurrentOutPtr[j];  //magnitude data
+            BufferFFTw1_mag[j][i] = hnd_cfft->CurrentOutPtr[j];  //magnitude data
 
 
-        for (j = 0; j < EQ64; j++) {       //input data for B2
-            CFFTin1Buff[j] = A2[j][ii];
-            CFFTin1Buff[j+1] = A2[j][ii+1];
+        for (j = 0; j < EQ64; j++) {       //input data for BufferFFTw2
+            CFFTin1Buff[j] = BufferFFTt2[j][ii];
+            CFFTin1Buff[j+1] = BufferFFTt2[j][ii+1];
         }
 
         CFFT_f32(hnd_cfft);             // calculate FFT
         CFFT_f32_mag(hnd_cfft);
 
         for (j = 0; j < EQ64; j++)
-            B2[i][j] = hnd_cfft->CurrentInPtr[j];  //complex data
+            //BufferFFTw2[i][j] = hnd_cfft->CurrentInPtr[j];  //complex data
+            BufferFFTw2[j][i] = hnd_cfft->CurrentInPtr[j];
 
         for (j = 0; j < EQ64/2; j++)
-            B2M[i][j] = hnd_cfft->CurrentOutPtr[j]; //magnitude data
+            //BufferFFTw2_mag[i][j] = hnd_cfft->CurrentOutPtr[j]; //magnitude data
+            BufferFFTw2_mag[j][i] = hnd_cfft->CurrentOutPtr[j]; //magnitude data
 
-//Step 2.1: Calculate magnitude sum x256
+        //Step 2.1: Calculate magnitude sum x256
         for (j = 0; j < EQ64/2; j++)   // summary magnitude matrix
-                C[i][j] = B1M[i][j] + B2M[i][j];
+            //Energy_0[i][j] = BufferFFTw1_mag[i][j] + BufferFFTw2_mag[i][j];
+            Energy_0[j][i] = BufferFFTw1_mag[j][i] + BufferFFTw2_mag[j][i];
     }
-    asm(" ESTOP0");
-//Step 3: Treshold
-    adaptive_threshold();
-
-//Step 4: Clustering
-    clustering();
-
-    asm(" ESTOP0");
-    while(1) {};
 }
 
 
-void clustering()
+void clustering_init(Uint16 * Stack[][EQ256], Uint16 * Mark[][2])
 {
-	// flgoritm
- //   https://habrahabr.ru/post/101338/
+    Uint16 i,j;
+    for (i = 0; i < MAX_STRUCT_SIZE; i++) {
+            Struct[i]->cluster = 0;
+            Struct[i]->energy = 0;
+            Struct[i]->distance = 0;
+            Struct[i]->velocity = 0;
+            Struct[i]->angle = 0;
+    }
 
+    for (i = 0; i < MAX_STACK_POINTER; i++) {
+        Stack[i][0] = 0;
+        Stack[i][1] = 0;
+    }
+
+    for (i = 0; i < EQ64/2; i++)
+        for (j = 0; j < EQ256; j++)
+            Mark[i][j] = 0;
 }
+
 
 void adaptive_threshold()
 {
-        Uint16 i,j, tr = 10;
+        Uint16 i,j, k = 10;
         float S1, S2;
         for (i = 0; i < EQ64/2; i++) //построчный обход
             for (j = 0; j < EQ256; j++)
             {
                 if (5-j <= 0 && j <= (EQ256-5)) {
-                    S1 = C[i][j-5] + C[i][j-4] + C[i][j-3];
+                    S1 = Energy_0[i][j-5] + Energy_0[i][j-4] + Energy_0[i][j-3];
                     S1 /= 3;
-                    S2 = C[i][j+3] + C[i][j+4] + C[i][j+5];
+                    S2 = Energy_0[i][j+3] + Energy_0[i][j+4] + Energy_0[i][j+5];
                     S2 /= 3;
-                    if ( (tr*S1 < C[i][j]) && (tr*S2 < C[i][j]) )
-                        D[i][j] = (Uint32) C[i][j];
+                    if ( (k*S1 < Energy_0[i][j]) && (k*S2 < Energy_0[i][j]) )
+                        Energy[i][j] = (Uint32) Energy_0[i][j];
                     else
-                        D[i][j] = 0;
+                        Energy[i][j] = 0;
                     continue;
                 }
                 if (j <= 2) {
-                    S2 = C[i][3+j] + C[i][4+j] + C[i][5+j];
+                    S2 = Energy_0[i][3+j] + Energy_0[i][4+j] + Energy_0[i][5+j];
                     S2 /= 3;
-                    if ( tr*S2 < C[i][j] )
-                        D[i][j] = (Uint32) C[i][j];
+                    if ( k*S2 < Energy_0[i][j] )
+                        Energy[i][j] = (Uint32) Energy_0[i][j];
                     else
-                        D[i][j] = 0;
+                        Energy[i][j] = 0;
                     continue;
                 }
                 if (j >= EQ256-2) {
-                    S1 = C[i][j-3] + C[i][j-4] + C[i][j-5];
+                    S1 = Energy_0[i][j-3] + Energy_0[i][j-4] + Energy_0[i][j-5];
                     S1 /= 3;
-                    if ( tr*S1 < C[i][j] )
-                        D[i][j] = (Uint32) C[i][j];
+                    if ( k*S1 < Energy_0[i][j] )
+                        Energy[i][j] = (Uint32) Energy_0[i][j];
                     else
-                        D[i][j] = 0;
+                        Energy[i][j] = 0;
                     continue;
                 }
                 if (j == 3) {
-                    S1 = C[i][j-3];
-                    S2 = C[i][j+3] + C[i][j+4] + C[i][j+5];
+                    S1 = Energy_0[i][j-3];
+                    S2 = Energy_0[i][j+3] + Energy_0[i][j+4] + Energy_0[i][j+5];
                     S2 /= 3;
-                    if ( (tr*S1 < C[i][j]) && (tr*S2 < C[i][j]) )
-                        D[i][j] = (Uint32) C[i][j];
+                    if ( (k*S1 < Energy_0[i][j]) && (k*S2 < Energy_0[i][j]) )
+                        Energy[i][j] = (Uint32) Energy_0[i][j];
                     else
-                        D[i][j] = 0;
+                        Energy[i][j] = 0;
                     continue;
                 }
                 if (j == 4) {
-                    S1 = C[i][j-3] + C[i][j-4];
+                    S1 = Energy_0[i][j-3] + Energy_0[i][j-4];
                     S1 /= 2;
-                    S2 = C[i][j+3] + C[i][j+4] + C[i][j+5];
+                    S2 = Energy_0[i][j+3] + Energy_0[i][j+4] + Energy_0[i][j+5];
                     S2 /= 3;
-                    if ( (tr*S1 < C[i][j]) && (tr*S2 < C[i][j]) )
-                        D[i][j] = (Uint32) C[i][j];
+                    if ( (k*S1 < Energy_0[i][j]) && (k*S2 < Energy_0[i][j]) )
+                        Energy[i][j] = (Uint32) Energy_0[i][j];
                     else
-                        D[i][j] = 0;
+                        Energy[i][j] = 0;
                     continue;
                 }
                 if (j == EQ256 - 3) {
-                    S1 = C[i][j-3] + C[i][j-4] + C[i][j-5];
+                    S1 = Energy_0[i][j-3] + Energy_0[i][j-4] + Energy_0[i][j-5];
                     S1 /= 3;
-                    S2 = C[i][j+3];
-                    if ( (tr*S1 < C[i][j]) && (tr*S2 < C[i][j]) )
-                        D[i][j] = (Uint32) C[i][j];
+                    S2 = Energy_0[i][j+3];
+                    if ( (k*S1 < Energy_0[i][j]) && (k*S2 < Energy_0[i][j]) )
+                        Energy[i][j] = (Uint32) Energy_0[i][j];
                     else
-                        D[i][j] = 0;
+                        Energy[i][j] = 0;
                     continue;
                 }
                 if (j == EQ256 - 4) {
-                    S1 = C[i][j-3] + C[i][j-4] + C[i][j-5];
+                    S1 = Energy_0[i][j-3] + Energy_0[i][j-4] + Energy_0[i][j-5];
                     S1 /= 3;
-                    S2 = C[i][j+3] + C[i][j+4];
+                    S2 = Energy_0[i][j+3] + Energy_0[i][j+4];
                     S2 /= 2;
-                    if ( (tr*S1 < C[i][j]) && (tr*S2 < C[i][j]) )
-                        D[i][j] = (Uint32) C[i][j];
+                    if ( (k*S1 < Energy_0[i][j]) && (k*S2 < Energy_0[i][j]) )
+                        Energy[i][j] = (Uint32) Energy_0[i][j];
                     else
-                        D[i][j] = 0;
+                        Energy[i][j] = 0;
                     continue;
                 }  /**/
             }
 }
+
+void argument()
+{
+    int i, j;
+    for(i = 0; i < EQ64/2; i++)
+        for(j = 0; j < EQ256; j+=2)
+            if(Energy[i][j] > 0)
+                Angle[i][j] = atan( BufferFFTw1[i][j+1]/BufferFFTw1[i][j] ) - atan( BufferFFTw2[i][j+1]/BufferFFTw2[i][j] );
+            else
+                Angle[i][j] = 0;
+}
+
+void clustering()
+{
+    Uint16 Mark[EQ64/2][EQ256];
+    Uint16 Stack[MAX_STACK_POINTER][2];
+    clustering_init(Stack, Mark);
+    Uint16 ii, jj, i, j, flag;
+    Uint16 cluster = 0;
+    Uint16 stack_pointer;
+
+    for (ii = 0; ii < EQ64; ii++)
+        for (jj = 0; jj < EQ64/2; jj++) {
+            stack_pointer = 1;
+            i = ii;
+            j = jj;
+            flag = 0;
+            while (stack_pointer > 0)
+            {
+                if (flag) {
+                    i = Stack[stack_pointer][0];
+                    j = Stack[stack_pointer][1];
+                }
+                flag = 1;
+
+                if ( j < EQ256-1 && i < EQ64/2-1 && Mark[i][j] == 0 && Energy[i][j] > 0 )
+                {
+                    Struct[cluster]->cluster = cluster;
+                    Struct[cluster]->energy += Energy_0(i,j);
+                    Struct[cluster]->distance += j*Energy_0(i,j);
+                    Struct[cluster]->velocity += (i - EQ64/4)*Energy_0(i,j);
+                    Struct[cluster]->angle += Angle(i,j)*Energy_0(i,j);
+
+                    Mark[i][j] = 1;
+                    stack_pointer--;
+
+                    if ( j + 1 < EQ256 && Mark[i][j+1] == 0 && Energy[i][j+1] > 0 ) // добавить условие угла
+                    {
+                        stack_pointer++;
+                        Stack[stack_pointer][0] = i;
+                        Stack[stack_pointer][1] = j+1;
+                    }
+                    if ( j - 1 >= 0 && Mark[i][j-1] == 0 && Energy[i][j-1] > 0 )
+                    {
+                        stack_pointer++;
+                        Stack[stack_pointer][0] = i;
+                        Stack[stack_pointer][1] = j-1;
+                    }
+                    if ( i + 1 < EQ64/2 && Mark[i+1][j] == 0 && Energy[i+1][j] > 0 )
+                    {
+                        stack_pointer++;
+                        Stack[stack_pointer][0] = i+1;
+                        Stack[stack_pointer][1] = j;
+                    }
+                    if ( i -1 >= 0 && Mark[i-1][j] == 0 && Energy[i-1][j] > 0 )
+                    {
+                        stack_pointer++;
+                        Stack[stack_pointer][0] = i-1;
+                        Stack[stack_pointer][1] = j;
+                    }
+                }
+                else
+                    break;
+            }
+            if (cluster < MAX_STUCT_SIZE && Struct[cluster].energy > 0)
+                cluster++;
+        }
+
+    for (i = 0; i < MAX_STUCT_SIZE; i++)
+        if (Struct[cluster].energy) > 0)
+        {
+            Struct[i][3] /= Struct(i,2);
+            Struct[i][4] /= Struct(i,2);
+            Struct[i][5] /= Struct(i,2);
+        }
+}
+
+/////////////////////////////////////////////////////////
 
 void timer0_init()
 {
@@ -255,7 +385,7 @@ void timer0_init()
     CpuTimer0Regs.TPR.all  = 0;  // Initialize pre-scale
     CpuTimer0Regs.TPRH.all = 0;
     CpuTimer0Regs.TCR.bit.TSS = 1;   // 1 = Stop timer
-    CpuTimer0Regs.TCR.bit.TRB = 1;   // 1 = reload timer
+    CpuTimer0Regs.TCR.bit.TRB = 1;   // 1 = Reload timer
     CpuTimer0Regs.TCR.all = 0x4000;
 }
 
