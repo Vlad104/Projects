0a2683c472c33758b87dc007bab27edb68b9c8c2
Vlad104 <vlad104khk@gmail.com>
2017-12-11 18:57:34 +0300

    Works on input signal

--- Matlab/m3/work/main.m
+++ Matlab/m3/work/main.m
@@ -12,6 +12,11 @@ EQ64 = 64; % число, эквивалентное 64
 BufferFFTt1 = fft(BufferIn1(:,:));
 BufferFFTt2 = fft(BufferIn2(:,:));
 
+%отсечка симметричной части
+%преобразуем [64][256] в [64][128]
+BufferFFTt1(EQ256/2+1:EQ256,:) = [];
+BufferFFTt2(EQ256/2+1:EQ256,:) = [];
+
 % операция транспонирования матрицы
 BufferFFTt1 = BufferFFTt1';
 BufferFFTt2 = BufferFFTt2';
@@ -26,13 +31,16 @@ energy0 = abs(BufferFFTw1) + abs(BufferFFTw2);
 k = 6; % коэффициент порога
 
 % адаптивная пороговая фильтрация
-energy = adaptive_filtering(energy0, k, EQ64, EQ256);
+energy = adaptive_filtering(energy0, k, EQ64, EQ256/2);
 
 % расчет угла
-angle = argument(BufferFFTw1, BufferFFTw2, energy, EQ64, EQ256);
+angle = argument(BufferFFTw1, BufferFFTw2, energy, EQ64, EQ256/2);
 
 % кластеризация и заполнение структуры
-Struct = clustering(energy, energy0, angle, EQ64, EQ256);
+Struct = clustering(energy, energy0, angle, EQ64, EQ256/2);
 %[Struct,MAT] = clustering(energy, energy0, angle, EQ64, EQ256);
 
 surfc(energy0); % построение 3D графика распределения мощностей
+title('Цели');
+xlabel('Дальность');
+ylabel('Скорость');
