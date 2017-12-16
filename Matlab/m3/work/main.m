1c0ad319bd71df90aa73276da841a64351355680
Vlad104 <vlad104khk@gmail.com>
2017-12-17 02:53:57 +0300

    Testing algorithm

--- Matlab/m3/work/main.m
+++ Matlab/m3/work/main.m
@@ -6,11 +6,11 @@ EQ64 = 64; % число, эквивалентное 64
  % на вход fft [EQ256][EQ64]
  % в такой записи fft делается по столбцам
  % на выходе fft [EQ256][EQ64]
-[BufferIn1, BufferIn2] = signal(EQ64, EQ256);
+[BufferIn1, BufferIn2] = signal_target4(EQ64, EQ256);
 
 % БПФ с прореживанием по времени
-BufferFFTt1 = fft(BufferIn1(:,:));
-BufferFFTt2 = fft(BufferIn2(:,:));
+BufferFFTt1 = fft(BufferIn1);
+BufferFFTt2 = fft(BufferIn2);
 
 %отсечка симметричной части
 %преобразуем [64][256] в [64][128]
@@ -22,8 +22,8 @@ BufferFFTt1 = BufferFFTt1';
 BufferFFTt2 = BufferFFTt2';
 
 % БПФ с прореживанием по частоте    
-BufferFFTw1 = fft(BufferFFTt1(:,:));
-BufferFFTw2 = fft(BufferFFTt2(:,:));
+BufferFFTw1 = fft(BufferFFTt1);
+BufferFFTw2 = fft(BufferFFTt2);
 
 % расчет мощности в каждой точке (сумма модулей соответствующих двух чисел)
 energy0 = abs(BufferFFTw1) + abs(BufferFFTw2);
@@ -40,7 +40,7 @@ angle = argument(BufferFFTw1, BufferFFTw2, energy, EQ64, EQ256/2);
 Struct = clustering(energy, energy0, angle, EQ64, EQ256/2);
 % [Struct,MAT] = clustering(energy, energy0, angle, EQ64, EQ256);
 
-surfc(energy0); % построение 3D графика распределения мощностей
+surfc(energy); % построение 3D графика распределения мощностей
 title('Цели');
 xlabel('Дальность');
 ylabel('Скорость');
