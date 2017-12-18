1cf623c1792e421cd222461e39d8e5e06a81d0d2
Vlad104 <vlad104khk@gmail.com>
2017-12-19 02:02:17 +0300

    Update signals and add f0git status

--- Matlab/m3/work/graf.m
+++ Matlab/m3/work/graf.m
@@ -1,62 +1,98 @@
-ZOND_g = abs(fft(ZOND));
-S1_RX_g = abs(fft(S1_RX));
-S2_RX_g = abs(fft(S2_RX));
-S1_SM_g = abs(fft(S1_SM));
-S2_SM_g = abs(fft(S2_SM));
-S1_g = abs(fft(S1));
-S2_g = abs(fft(S2));
-
-ZOND_g(25000/2+1:25000,:) = [];
-S1_RX_g(25000/2+1:25000,:) = [];
-S2_RX_g(25000/2+1:25000,:) = [];
-S1_SM_g(25000/2+1:25000,:) = [];
-S2_SM_g(25000/2+1:25000,:) = [];
-S1_g(256/2+1:256,:) = [];
-S2_g(256/2+1:256,:) = [];
-
 plot(ZOND);
 title('Зондирующий сигнал');
 xlabel('Частота, Гц');
-ylabel('Амплитуда, В');
+ylabel('Амплитуда');
 
 figure
-semilogx(ZOND_g);
+pwelch(ZOND);
 title('Спектр зондирующего сигнала');
-xlabel('Частота, Гц');
-ylabel('Амплитуда, В');
 
 figure
-semilogx(S1_RX_g);
+pwelch(S1_RX);
 title('Спектр принятого сигнала');
-xlabel('Частота, Гц');
-ylabel('Амплитуда, В');
 
 figure
-semilogx(S2_RX_g);
+pwelch(S2_RX);
 title('Спектр принятого сигнала');
-xlabel('Частота, Гц');
-ylabel('Амплитуда, В');
 
 figure
-semilogx(S1_SM_g);
+pwelch(S1_SM);
 title('Спектр сигнала ны выходе смесителя');
-xlabel('Частота, Гц');
-ylabel('Амплитуда, В');
 
 figure
-semilogx(S2_SM_g);
+pwelch(S2_SM);
 title('Спектр сигнала ны выходе смесителя');
-xlabel('Частота, Гц');
-ylabel('Амплитуда, В');
 
 figure
-semilogx(S1_g);
+pwelch(S1);
 title('Спектр дискретизированного сигнала');
-xlabel('Частота, Гц');
-ylabel('Амплитуда, В');
 
 figure
-semilogx(S2_g);
+pwelch(S2);
 title('Спектр дискретизированного сигнала');
-xlabel('Частота, Гц');
-ylabel('Амплитуда, В');
+
+
+
+
+% ZOND_g = abs(fft(ZOND));
+% S1_RX_g = abs(fft(S1_RX));
+% S2_RX_g = abs(fft(S2_RX));
+% S1_SM_g = abs(fft(S1_SM));
+% S2_SM_g = abs(fft(S2_SM));
+% S1_g = abs(fft(S1));
+% S2_g = abs(fft(S2));
+% 
+% ZOND_g(25000/2+1:25000,:) = [];
+% S1_RX_g(25000/2+1:25000,:) = [];
+% S2_RX_g(25000/2+1:25000,:) = [];
+% S1_SM_g(25000/2+1:25000,:) = [];
+% S2_SM_g(25000/2+1:25000,:) = [];
+% S1_g(256/2+1:256,:) = [];
+% S2_g(256/2+1:256,:) = [];
+% 
+% plot(ZOND);
+% title('Зондирующий сигнал');
+% xlabel('Частота, Гц');
+% ylabel('Амплитуда, В');
+% 
+% figure
+% semilogx(ZOND_g);
+% title('Спектр зондирующего сигнала');
+% xlabel('Частота, Гц');
+% ylabel('Амплитуда, В');
+% 
+% figure
+% semilogx(S1_RX_g);
+% title('Спектр принятого сигнала');
+% xlabel('Частота, Гц');
+% ylabel('Амплитуда, В');
+% 
+% figure
+% semilogx(S2_RX_g);
+% title('Спектр принятого сигнала');
+% xlabel('Частота, Гц');
+% ylabel('Амплитуда, В');
+% 
+% figure
+% semilogx(S1_SM_g);
+% title('Спектр сигнала ны выходе смесителя');
+% xlabel('Частота, Гц');
+% ylabel('Амплитуда, В');
+% 
+% figure
+% semilogx(S2_SM_g);
+% title('Спектр сигнала ны выходе смесителя');
+% xlabel('Частота, Гц');
+% ylabel('Амплитуда, В');
+% 
+% figure
+% semilogx(S1_g);
+% title('Спектр дискретизированного сигнала');
+% xlabel('Частота, Гц');
+% ylabel('Амплитуда, В');
+% 
+% figure
+% semilogx(S2_g);
+% title('Спектр дискретизированного сигнала');
+% xlabel('Частота, Гц');
+% ylabel('Амплитуда, В');
