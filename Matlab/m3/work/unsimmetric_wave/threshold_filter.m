e44dca358b21e860fa7b6e5fa17360e48b405015
Vlad104 <vlad104khk@gmail.com>
2018-02-14 19:06:02 +0300

    made filtering mod easier

---
+++ Matlab/m3/work/unsimmetric_wave/threshold_filter.m
@@ -0,0 +1,39 @@
+function energy = threshold_filter(energy0, k, EQ64, EQ256, n_look, n_miss)
+%	функция пороговой фильтрации входной матрицы
+%   вход:
+%       - energy0 - матрица EQ64хEQ256 значений мощности в каждой точке
+%       - k - пороговый коэффициен, подбирается эксперементальным методом
+%       - EQ256 и EQ64 - эквиваленты чисел 256 и 64 соответственног
+%       - n_look - число точек для расчета среднего значения
+%       - n_miss - число отступаемых точек
+%   выход:
+%       - energy - матрица EQ64хEQ256 значений мощности в точках, со
+%         значением выше порога и 0 в точках ниже порога
+
+    %инициализация переменных
+    energy(1:EQ64, 1:EQ256) = 0; % создание массива с нулями
+    n = n_look + n_miss;
+    temp(1:EQ64+2*n) = 0;
+    S1 = 0; S2 = 0; % переменные для дальнейшего расчета "плеч"
+    
+    for i = 1:EQ64
+        temp(1:n) =  energy(i,EQ256:-1:EQ256-n);
+        temp(n:EQ256+n) = energy(i,1:EQ256);
+        temp(EQ256+n:EQ256+2*n) = energy(i,1:n);
+        for j = n+1:EQ256+n
+            S1 = 0;
+            S2 = 0;
+            for z = 1:n_look
+                S1 = S1 + temp(i,j+n_miss+n_look); 
+                S2 = S2 + temp(i,j-n_miss-n_look); 
+            end;
+            S1 = S1/n_look;
+            S2 = S2/n_look;
+            if ( j >= n+1 && j <= EQ256+n && temp(i,j) > k*S1 && temp(i,j) > k*S2 )
+                energy(i,j-n) = temp(i,j);
+            else
+                energy(i,j-n) = 0;
+            end;                
+        end;      
+    end;
+end
